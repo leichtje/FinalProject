33bef0445045887276a94af94b732dfdfc5ef409
Joel7721 <mensahjl@mail.uc.edu>
2023-11-28 20:01:50 -0500

    t

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<MemberContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("MemberContext")));
+builder.Services.AddDbContext<MemberContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MemberContext")));
 
 builder.Services.AddSwaggerDocument();
 
